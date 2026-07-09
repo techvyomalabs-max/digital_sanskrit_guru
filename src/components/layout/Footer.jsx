3762015b2c619779b5779d346c8a1190240f8846
supreethvyoma <supreeth.vyoma@gmail.com>
2026-07-09 15:35:22 +0530

    Optimize page heights on mobile views for login, register, and wishlist pages to prevent vertical overflow

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,11 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="site-footer">
-      <p>© 2026 Digital Sanskrit Guru</p>
+      <p>© All Copyrights Reserved "Vyoma Linguistic Labs Foundation".</p>
+      <div class="footer-links">
+        <a href="privacy.html">Privacy Policy</a>
+        <a href="terms_and_condtions.html">Terms & conditions</a>
+      </div>
     </footer>
   );
 }
